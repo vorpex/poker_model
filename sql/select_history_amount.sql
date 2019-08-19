4810adb832e1ed91bb2e4d3f529197460f90a676
vorpex <vorpex@freemail.hu>
2019-08-19 04:31:38 +0200

    update

--- sql/select_history_amount.sql
+++ sql/select_history_amount.sql
@@ -1,64 +1,31 @@
 SELECT
-  SUM(s.sum_amount) AS sum_amount
+  h1.amount
 
-FROM (
+FROM
+  poker.history h1
+
+WHERE
+  1 = 1
+  AND h1.move IN ('small_blind', 'big_blind', 'raise')
+  AND h1.game_id = {GAME_ID}
+  AND h1.phase = {PHASE}
+  AND h1.nr > (
 
   SELECT
-    'raise' AS move_type,
-    SUM(NVL(h1.amount, 0)) AS sum_amount
+    NVL(MAX(h2.nr), -1) AS max_player_nr
 
   FROM
-    poker.history h1
+    poker.history h2
 
   WHERE
     1 = 1
-    AND h1.move = 'raise'
-    AND h1.game_id = {GAME_ID}
-    AND h1.phase = {PHASE}
-    AND h1.nr > (
+    AND h2.game_id = {GAME_ID}
+    AND h2.phase = {PHASE}
+    AND h2.player_name = '{PLAYER_NAME}'
 
-    SELECT
-      NVL(MAX(h2.nr), -1) AS max_player_nr
+)
 
-    FROM
-      poker.history h2
+ORDER BY
+  h1.nr DESC
 
-    WHERE
-      1 = 1
-      AND h2.game_id = {GAME_ID}
-      AND h2.phase = {PHASE}
-      AND h2.player_name = '{PLAYER_NAME}'
-
-    )
-
-  UNION
-
-  SELECT
-    'blind' AS move_type,
-    MAX(NVL(h3.amount, 0)) AS sum_amount
-
-  FROM
-    poker.history h3
-
-  WHERE
-    1 = 1
-    AND h3.move IN ('small_blind', 'big_blind')
-    AND h3.game_id = {GAME_ID}
-    AND h3.phase = {PHASE}
-    AND h3.nr > (
-
-    SELECT
-      NVL(MAX(h4.nr), -1) AS max_player_nr
-
-    FROM
-      poker.history h4
-
-    WHERE
-      1 = 1
-      AND h4.game_id = {GAME_ID}
-      AND h4.phase = {PHASE}
-      AND h4.player_name = '{PLAYER_NAME}'
-
-    )
-
-) s
+LIMIT 1
